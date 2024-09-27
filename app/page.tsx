9d667d9d52f680637d5bbada5755aa12229a359a
C974 <am2104114@qu.edu.qa>
2024-09-27 14:13:54 +0300

    up

--- app/page.tsx
+++ app/page.tsx
@@ -708,7 +708,6 @@
 //     </div>
 //   )
 // }
-
 'use client'
 
 import React, { useRef, ReactNode } from 'react'
@@ -726,13 +725,16 @@ const FadeInSection: React.FC<FadeInSectionProps> = ({ children }) => {
     offset: ["start end", "end start"]
   })
 
-  const opacity = useTransform(scrollYProgress, [0, 0.5], [0, 1])
-  const y = useTransform(scrollYProgress, [0, 0.5], [50, 0])
+  // Updated transforms for smoother animation and scaling
+  const opacity = useTransform(scrollYProgress, [0, 0.3], [0, 1])
+  const y = useTransform(scrollYProgress, [0, 0.3], [60, 0])
+  const scale = useTransform(scrollYProgress, [0, 0.3], [0.95, 1])
 
   return (
     <motion.div
       ref={ref}
-      style={{ opacity, y }}
+      style={{ opacity, y, scale }}
+      transition={{ duration: 0.8, ease: "easeOut" }} // Updated timing and easing
     >
       {children}
     </motion.div>
